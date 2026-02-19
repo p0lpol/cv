6ef88df9758684a94204d27c22ca23c9ff0e3cb4
Delavictoire Paul-Arnaud <paul-arnaud.delavictoire.etu@univ-lille.fr>
2026-02-19 13:32:54 +0100

    Update ProfileCard layout and content
    - Enhanced styling with adjusted shadow and width.
    - Modified text sizes and image dimensions for improved design.
    - Added a paragraph section for additional profile information.

--- src/components/ProfileCard.tsx
+++ src/components/ProfileCard.tsx
@@ -7,9 +7,14 @@ interface ProfileCardProps {
 export default function ProfileCard({ name, image} : ProfileCardProps){
     return (
         <div className="flex flex-col items-center justify-evenly w-[50vw] h-[60vh] rounded-3xl border-2
-        border-gray-900 px-8 shadow-xl bg-slate-900 text-white shadow-slate-800">
-            <img src={image} alt={name} className="rounded-full w-2/3 h-auto"/>
-            <h1 className="text-4xl text-center wrap-break-word">{name}</h1>
+        border-gray-900 px-8 shadow-[8px_8px_6px_0px_rgba(0,_0,_0,_0.1)] bg-slate-900 text-white shadow-slate-800 @container">
+            <img src={image} alt={name} className="rounded-full w-64"/>
+            <h1 className="text-5xl text-center wrap-break-word">{name}</h1>
+            <p className="text-sm text-justify">
+                Je suis étudiant en 2eme année de BUT informatique, en parcours réalisation d'applications.
+                Je recherche actuellement un <strong>stage de dix semaines</strong> à partir du <strong>6 avril</strong> ainsi qu'une <strong>alternance d'un an</strong> pour <strong>septembre</strong> de l'année prochaine.
+                N'hésitez pas à me contacter pour toute information complémentaire !
+            </p>
         </div>
     )
 }

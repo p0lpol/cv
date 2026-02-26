eb0176e69a575c4753e83b8c6dd2082863a8b256
Paul-Arnaud Delavictoire <paul-arnaud.delavictoire.etu@univ-lille.fr>
2026-02-26 19:10:45 +0100

    Update `Financial` project page title and description to reflect Spring/React development focus

--- app/projets/financial/page.tsx
+++ app/projets/financial/page.tsx
@@ -243,8 +243,8 @@ export default function Bac() {
 
     return (
         <div className="flex flex-col flex-1 w-full items-center mb-15">
-            <h1 className="text-white text-5xl font-bold font-serif p-8" >Labyrinthe</h1>
-            <h2 className="text-white text-3xl font-bold p-8">Projet de Développement Java / JavaFX</h2>
+            <h1 className="text-white text-5xl font-bold font-serif p-8" >Financial</h1>
+            <h2 className="text-white text-3xl font-bold p-8">Projet de Développement Spring / React</h2>
 
             <div className="flex flex-1 w-full items-start">
                 <ProjectBar/>

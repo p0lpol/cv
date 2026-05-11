8d209a7cce6c0bff3b5ba95e5847afed63af8140
Paul-Arnaud Delavictoire <delavictoirepaul5@gmail.com>
2026-05-11 08:24:24 +0400

    Fixed packages + download button width

--- app/page.tsx
+++ app/page.tsx
@@ -9,7 +9,7 @@ export default function Home() {
         />
             <div className="h-12"/>
         <a href="/cv.pdf" download="Paul-Arnaud_DELAVICTOIRE" className="flex justify-center
-        items-center bg-slate-900 hover:bg-slate-800 text-white font-bold p-4 rounded-full w-full text-2xl
+        items-center bg-slate-900 hover:bg-slate-800 text-white font-bold p-4 rounded-full w-1/3 text-2xl
         hover:scale-110 transition-all duration-200">
             <button id="download" type="button">Télécharger mon CV</button>
         </a>

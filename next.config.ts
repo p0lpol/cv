d8f4a1826e65b2626a833a956f689aec4401dcdb
Paul-Arnaud Delavictoire <delavictoirepaul5@gmail.com>
2026-05-29 13:54:59 +0400

    Fixed next config for deployment

--- next.config.ts
+++ next.config.ts
@@ -1,16 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    output: 'export',
     images: {
+        unoptimized: true,
         remotePatterns: [
             {
                 protocol: 'https',
                 hostname: 'upload.wikimedia.org',
                 pathname: '/**',
-            },
-            {
-                protocol: 'https',
-                hostname: 'picsum.photos',
-                pathname: '/**'
             }
         ],
     },
